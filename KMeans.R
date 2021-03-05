95e088191b238356461b1b49efd52a9988188f17
Ammar <ammarraneez@gmail.com>
2021-03-06 00:55:51 +0530

    confusion matrix generated and models evaluated

--- KMeans.R
+++ KMeans.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(factoextra)
 library(NbClust)
+#for evaluations
 library(caret)
 library(ggplot2)
 library(cluster)
@@ -12,6 +13,7 @@ vehicle_data <- read_excel("../../vehicles.xlsx")
 vehicle_data <- vehicle_data[c(-1)] 
 #data type
 class(vehicle_data)
+#summary(vehicle_data)
 #any missing values?
 sum(is.na(vehicle_data))
 
@@ -61,7 +63,7 @@ boxplot(scaled_vehicle_data_inputs, las = 2, col = c("lightgreen", "lightblue"),
 #Manual
 manual_cluster_size <- 4
 #Elbow Method
-fviz_nbclust(scaled_vehicle_data_inputs, kmeans, method = "wss") geo_p + labs(subtitle = "Elbow")
+fviz_nbclust(scaled_vehicle_data_inputs, kmeans, method = "wss") + labs(subtitle = "Elbow")
 #Silhouette Method
 fviz_nbclust(scaled_vehicle_data_inputs, kmeans, method = "silhouette") + labs(subtitle = "Silhouette")
 #Gap statistic Method
@@ -69,28 +71,40 @@ fviz_nbclust(scaled_vehicle_data_inputs, kmeans, method = "gap_stat", verbose = 
 
 
 ## KMEANS ANALYSIS ##
+#set seed for reproducible runs
 set.seed(101)
+
 #Manual
 manual_kmean <- kmeans(scaled_vehicle_data_inputs, centers = manual_cluster_size, nstart = 50)
 clusplot(scaled_vehicle_data, manual_kmean$cluster, color = T, shade = T, labels = 4)
-comparison_table_manual <- table(manual_kmean$cluster, scaled_vehicle_data_output$Class)
+comparison_table_manual <- table(scaled_vehicle_data_output$Class, manual_kmean$cluster)
 
 set.seed(102)
 #Elbow
-elbow_kmean <- kmeans(scaled_vehicle_data_inputs, centers = 4, nstart = 50)
+elbow_kmean <- kmeans(scaled_vehicle_data_inputs, centers = 3, nstart = 50)
 clusplot(scaled_vehicle_data, elbow_kmean$cluster, color = T, shade = T, labels = 4)
-comparison_table_elbow <- table(elbow_kmean$cluster, scaled_vehicle_data_output$Class)
+comparison_table_elbow <- table(scaled_vehicle_data_output$Class, elbow_kmean$cluster)
 
 set.seed(103)
 #Silhouette & Gap Stat
 silhouette_gap_kmean <- kmeans(scaled_vehicle_data_inputs, centers = 2, nstart = 50)
 clusplot(scaled_vehicle_data, silhouette_gap_kmean$cluster, color = T, shade = T, labels = 4)
-comparison_table_silhouette_gap <- table(silhouette_gap_kmean$cluster, scaled_vehicle_data_output$Class)
+comparison_table_silhouette_gap <- table(scaled_vehicle_data_output$Class, silhouette_gap_kmean$cluster)
 
-#Visualize data and classes
-ggplot(scaled_vehicle_data, aes(Rad.Ra, Pr.Axis.Ra)) + geom_point(aes(col=Class), size = 3)
 
 #table evaluations for each method
-comparison_table_manual
-comparison_table_elbow
-comparison_table_silhouette_gap
+#comparison_table_manual
+#comparison_table_elbow
+#comparison_table_silhouette_gap
+
+#convert to factors, since confusion matrix expects factors
+ground_truth_as_factor <- as.factor(scaled_vehicle_data_output$Class)
+#predictions_as_factor <- as.factor(manual_kmean$cluster)
+#predictions_as_factor <- as.factor(elbow_kmean$cluster)
+predictions_as_factor <- as.factor(silhouette_gap_kmean$cluster)
+
+#since the predictions comes out as 1,2,3,4, The categorical data of the ground truth is converted to numerical
+ground_truth_as_numeric <- as.numeric(ground_truth_as_factor)
+#the data type has to be changed back to factor, since CM can accept only factors
+ground_truth_as_factor <- as.factor(ground_truth_as_numeric)
+confusionMatrix(data = predictions_as_factor, reference = ground_truth_as_factor, mode = 'prec_recall')
