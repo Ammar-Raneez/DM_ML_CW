4718d1f142653c9d0f5030e7c9314ae622d4dfe3
Ammar <ammarraneez@gmail.com>
2021-03-08 00:47:39 +0530

    remove trends and seasonality

--- TimeSeries.R
+++ TimeSeries.R
@@ -1,4 +1,21 @@
 library(readxl)
+library(fpp2)
 
 exchange_data <- read_excel("../../ExchangeUSD.xlsx")
-summary(exchange_data)
+
+#remove "wdy" column
+exchange_data <- exchange_data[c(-2, -1)] 
+
+#make the data time series
+exchange_data <- ts(exchange_data)
+
+#plot time series
+autoplot(exchange_data) + ggtitle("Time Plot: Exchange Rates")
+
+#get the difference to make the data trend-stationary
+DIF <- diff(exchange_data)
+autoplot(DIF) + ggtitle("Time Plot: Change in Exchange Rates")
+
+#check for any seasonality
+ggseasonplot(DIF) + ggtitle("Season Plot: Change in Exchange Rates")
+#not seasonal
