bb4c191114857b1b44af3bd4f8f6d6f97fa0ab8a
SABIQ <ammarraneez@gmail.com>
2021-04-27 14:05:49 +0530

    added code comments and removed unneeded code

--- KMeans.R
+++ KMeans.R
@@ -1,15 +1,14 @@
 library(readxl)
 library(factoextra)
 library(NbClust)
-#for evaluations
 library(caret)
 library(ggplot2)
 library(cluster)
 
 
-#read in the data
+# read in the data
 vehicle_data <- read_excel("../../vehicles.xlsx")
-#remove samples column due to it only being a counter
+# remove samples column due to it only being a counter
 vehicle_data <- vehicle_data[c(-1)] 
 # data type
 class(vehicle_data)
@@ -19,32 +18,33 @@ summary(vehicle_data)
 sum(is.na(vehicle_data))
 
 
+
 ## DATA PREPROCESSING SECTION ##
-#normalize the input features
+# normalize the input features
 scaled_vehicle_data <- apply(vehicle_data[-c(ncol(vehicle_data))], 2, scale)
-#bind the class column back into the normalized data set
+# bind the class column back into the normalized data set
 scaled_vehicle_data <- cbind(scaled_vehicle_data, vehicle_data[c(ncol(vehicle_data))])
 
-#visualize outliers of data set
+# visualize outliers of data set
 boxplot(scaled_vehicle_data[-c(ncol(vehicle_data))], las = 2, col = c("lightgreen", "lightblue"), ylab = "Normalized Values")
-#Based on box plot, following columns have outliers
+# Based on box plot, following columns have outliers
 have_outliers <- c("Rad.Ra", "Pr.Axis.Ra", "Max.L.Ra", "Pr.Axis.Rect", "Sc.Var.Maxis", "Sc.Var.maxis", "Skew.Maxis", "Skew.maxis", "Kurt.maxis")
 
-#loop through all the columns that have outliers
+# loop through all the columns that have outliers
 for (outlier in have_outliers) {
   number_of_rows = nrow(scaled_vehicle_data)
   
-  #get quantile values - 25th and 75th percentiles
+  # get quantile values - 25th and 75th percentiles
   quant <- quantile(scaled_vehicle_data[, outlier], probs = c(0.25, 0.75))
   
-  #calculate IQR for the respective columns
+  # calculate IQR for the respective columns
   iqr <- IQR(scaled_vehicle_data[1:number_of_rows, outlier])
   
-  #An outlier is a point below lower quartile and above upper quartile
+  # An outlier is a point below lower quartile and above upper quartile
   upper_quartile <- quant[2] + 1.5*iqr
   lower_quartile <- quant[1] - 1.5*iqr
   
-  #subset the data set, such that the outliers are filtered out
+  # subset the data set, such that the outliers are filtered out
   scaled_vehicle_data <- subset(
     scaled_vehicle_data, 
     scaled_vehicle_data[c(-ncol(vehicle_data))][, outlier] > lower_quartile & 
@@ -52,63 +52,66 @@ for (outlier in have_outliers) {
   ) 
 }
 
-#data set can now be split into input and output
+# data set can now be split into input and output
 scaled_vehicle_data_inputs <- scaled_vehicle_data[c(-ncol(vehicle_data))]
 scaled_vehicle_data_output <- scaled_vehicle_data[c(ncol(vehicle_data))]
 
-#plot box plot again to check whether outliers have been removed
+# plot box plot again to check whether outliers have been removed
 boxplot(scaled_vehicle_data_inputs, las = 2, col = c("lightgreen", "lightblue"), ylab = "Normalized Values")
 
 
+
 ## KMEANS CLUSTER DEFINITION ##
-#Manual
+# Manual
 manual_cluster_size <- 4
-#Elbow Method
+# Elbow Method
 fviz_nbclust(scaled_vehicle_data_inputs, kmeans, method = "wss") + labs(subtitle = "Elbow")
-#Silhouette Method
+# Silhouette Method
 fviz_nbclust(scaled_vehicle_data_inputs, kmeans, method = "silhouette") + labs(subtitle = "Silhouette")
-#Gap statistic Method
+# Gap statistic Method
 fviz_nbclust(scaled_vehicle_data_inputs, kmeans, method = "gap_stat", verbose = F) + labs(subtitle = "Gap statistic")
-#method of majority
+# method of majority
 NbClust(data = scaled_vehicle_data_inputs, distance = "euclidean", min.nc = 2, max.nc = 15, method = "kmeans")
 
 
+
 ## KMEANS ANALYSIS ##
-#set seed for reproducible runs
+# set seed for reproducible runs
 set.seed(101)
 
-#Manual
+# Manual
 manual_kmean <- kmeans(scaled_vehicle_data_inputs, centers = manual_cluster_size, nstart = 50)
 clusplot(scaled_vehicle_data, manual_kmean$cluster, color = T, shade = T, labels = 4)
 comparison_table_manual <- table(scaled_vehicle_data_output$Class, manual_kmean$cluster)
 
 set.seed(102)
-#Elbow and NbClust proposed 3 clusters
+# Elbow and NbClust proposed 3 clusters
 elbow_kmean <- kmeans(scaled_vehicle_data_inputs, centers = 3, nstart = 50)
 clusplot(scaled_vehicle_data, elbow_kmean$cluster, color = T, shade = T, labels = 4)
 comparison_table_elbow <- table(scaled_vehicle_data_output$Class, elbow_kmean$cluster)
 
 set.seed(103)
-#Silhouette & Gap Stat proposed 2 clusters
+# Silhouette & Gap Stat proposed 2 clusters
 silhouette_gap_kmean <- kmeans(scaled_vehicle_data_inputs, centers = 2, nstart = 50)
 clusplot(scaled_vehicle_data, silhouette_gap_kmean$cluster, color = T, shade = T, labels = 4)
 comparison_table_silhouette_gap <- table(scaled_vehicle_data_output$Class, silhouette_gap_kmean$cluster)
 
 
-#table evaluations for each method
+
+## KMEANS EVALUATION ##
 comparison_table_manual
 comparison_table_elbow
 comparison_table_silhouette_gap
 
-#convert to factors, since confusion matrix expects factors
+# convert to factors, since confusion matrix expects factors
 ground_truth_as_factor <- as.factor(scaled_vehicle_data_output$Class)
 predictions_as_factor_manual <- as.factor(manual_kmean$cluster)
 predictions_as_factor_elbow <- as.factor(elbow_kmean$cluster)
 predictions_as_factor_sg <- as.factor(silhouette_gap_kmean$cluster)
 
-#since the predictions comes out as 1,2,3,4, The categorical data of the ground truth is converted to numerical
+# since the predictions comes out as 1,2,3,4, The categorical data of the ground truth is converted to numerical
 ground_truth_as_numeric <- as.numeric(ground_truth_as_factor)
-#the data type has to be changed back to factor, since CM can accept only factors
+# the data type has to be changed back to factor, since CM can accept only factors
 ground_truth_as_factor <- as.factor(ground_truth_as_numeric)
 # plot confusion matrix for all
 confusionMatrix(data = predictions_as_factor_manual, reference = ground_truth_as_factor, mode = 'prec_recall')
--- TimeSeries.R
+++ TimeSeries.R
@@ -7,6 +7,7 @@ library(useful)
 library(nnfor)
 library(forecast)
 
+
 # import dataset
 exchange_data <- read_excel("../../ExchangeUSD.xlsx")
 # remove unneeded days column
@@ -25,10 +26,6 @@ maxs <- apply(exchange_data, 2, max)
 mins <- apply(exchange_data, 2, min)
 scaled_exchange_data <- as.data.frame(scale(exchange_data, center = mins, scale = maxs - mins))
 
-# a scaling performed in the mlp() function of R
-scaled_rates <- linscale(exchange_data$rate, minmax=list("mn"=-.8,"mx"=0.8))
-scaled_exchange_data$rate <- scaled_rates$x
-
 
 # plot partial autocorrelation plot to check for optimum order of AR ~ however many orders
 # will be tested upon
@@ -38,26 +35,24 @@ plot(exchange_data[c(2)])
 
 
 
-
-### TWO HIDDEN LAYERS
-# find optimum order of AR(p) model
+## TWO HIDDEN LAYERS ##
 perform_neuralnet_ar_two <- function(seed, order, new_col_name) {
-  # shift the column downwards, {order} number of times to create time delayed lags
+  # write a function to obtain the optimum order of AR(p) model #
+  # will create different input/output matrices, based on the specific order (the lag column changes)
+  # will then remove the unneeded date column, split the data into train and test sets, of fixed train set sizes
+  # will then create and train a neural net of internal conditions in control form, so only the input/output matrix would affect evaluation
+  # will then predict on the test set, and return the RMSE, MAE and MAPE of the predictions
+  
   scaled_exchange_data_shifted <- shift.column(scaled_exchange_data, columns = "rate", newNames = new_col_name, len = order, up = F)
-  # remove unneeded date column
   scaled_exchange_data_shifted <- scaled_exchange_data_shifted[c(-1)]
-  # split data into train and test sets - first 400 rows always train, remaining rows test, and remove rows that have the lag column as null
   train <- na.omit(scaled_exchange_data_shifted[0:400, ])
   test_index = 400 + order
   test <- na.omit(scaled_exchange_data_shifted[test_index:500, ])
   
-  # set a seed, for reproducibility  and use a fixed neural net structure to obtain best order of AR(p)
   set.seed(seed) 
-  # create a common neural net and get predictions
-  nn_trial <- neuralnet(rate ~ rate_5, data = train, hidden = c(6, 6), act.fct = "logistic", err.fct = "sse", lifesign = "full", learningrate = 0.08, rep = 10, linear.output = T)
+  nn_trial <- neuralnet(rate ~ rate_1, data = train, hidden = c(6, 6), act.fct = "logistic", err.fct = "sse", lifesign = "full", learningrate = 0.08, rep = 10, linear.output = T)
   nn_trial.test_prediction <- predict(nn_trial, test)
   
-  # evaluations using RMSE, MAE and MAPE
   rmse_nn_trial <- round(rmse(test[, 1], nn_trial.test_prediction), digits = 4)
   mae_nn_trial <- round(mae(test[, 1], nn_trial.test_prediction), digits = 4)
   mape_nn_trial <- round(mape(test[, 1], nn_trial.test_prediction), digits = 4)
@@ -69,8 +64,7 @@ order_2_hidden_2 <- perform_neuralnet_ar_two(104, 2, "rate_2")
 order_3_hidden_2 <- perform_neuralnet_ar_two(104, 3, "rate_3")
 order_4_hidden_2 <- perform_neuralnet_ar_two(104, 4, "rate_4")
 order_5_hidden_2 <- perform_neuralnet_ar_two(104, 5, "rate_5")
-# AR of order 1 gave the best metrics
-
+# AR of order 1 gave the best metrics, as determined in PACF
 
 
 # Optimal Input vector - AR(1)
@@ -79,8 +73,11 @@ scaled_exchange_data_hidden_2_shifted <- scaled_exchange_data_hidden_2_shifted[c
 train_hidden_2 <- na.omit(scaled_exchange_data_hidden_2_shifted[0:400, ])
 test_hidden_2 <- na.omit(scaled_exchange_data_hidden_2_shifted[401:500, ])
 
-# implement many neural networks with different hyper parameter values
-perfom_neuralnet_calculations_two <- function(seed, hidden, activation, learning) {
+
+perform_neuralnet_calculations_two <- function(seed, hidden, activation, learning) {
+  # write a function that will determine the best internal neural net structure and hyper parameters #
+  # will train on the input/output matrix of AR(1) and vary inner neural net values
+  # will then predict on the test set and return the RMSE, MAE, MAPE of the predictions
   set.seed(seed) 
   nn_trial <- neuralnet(rate ~ rate_1, data = train_hidden_2, hidden = c(hidden, hidden), act.fct = activation, err.fct = "sse", lifesign = "full", learningrate = learning, rep = 10, linear.output = T)
   nn_trial.test_prediction <- predict(nn_trial, test_hidden_2)
@@ -93,28 +90,28 @@ perfom_neuralnet_calculations_two <- function(seed, hidden, activation, learning
 }
 
 # find optimal number of nodes for 2 hidden layers
-trial0_metrics <- perfom_neuralnet_calculations_two(104, 2, "logistic", 0.08)
-trial1_metrics <- perfom_neuralnet_calculations_two(104, 3, "logistic", 0.08)
-trial2_metrics <- perfom_neuralnet_calculations_two(104, 4, "logistic", 0.08)
-trial3_metrics <- perfom_neuralnet_calculations_two(104, 5, "logistic", 0.08)
-trial4_metrics <- perfom_neuralnet_calculations_two(104, 6, "logistic", 0.08) 
-trial5_metrics <- perfom_neuralnet_calculations_two(104, 8, "logistic", 0.08)
+trial0_metrics <- perform_neuralnet_calculations_two(104, 2, "logistic", 0.08)
+trial1_metrics <- perform_neuralnet_calculations_two(104, 3, "logistic", 0.08)
+trial2_metrics <- perform_neuralnet_calculations_two(104, 4, "logistic", 0.08)
+trial3_metrics <- perform_neuralnet_calculations_two(104, 5, "logistic", 0.08)
+trial4_metrics <- perform_neuralnet_calculations_two(104, 6, "logistic", 0.08) 
+trial5_metrics <- perform_neuralnet_calculations_two(104, 8, "logistic", 0.08)
 
 # find best learning rate value for above best trial
-trial6_metrics <- perfom_neuralnet_calculations_two(104, 4, "logistic", 0.04)
-trial7_metrics <- perfom_neuralnet_calculations_two(104, 4, "logistic", 0.02)
-trial8_metrics <- perfom_neuralnet_calculations_two(104, 4, "logistic", 0.1)
+trial6_metrics <- perform_neuralnet_calculations_two(104, 4, "logistic", 0.04)
+trial7_metrics <- perform_neuralnet_calculations_two(104, 4, "logistic", 0.02)
+trial8_metrics <- perform_neuralnet_calculations_two(104, 4, "logistic", 0.1)
 
 # find optimal activation function for above best trial
-trial9_metrics <- perfom_neuralnet_calculations_two(104, 4, "tanh", 0.08)
-### Optimal internal network values values for 2 hidden layer network with seed 104: (4, 4), logistic, 0.08, trial2
+trial9_metrics <- perform_neuralnet_calculations_two(104, 4, "tanh", 0.08)
+# Optimal internal network values values for 2 hidden layer network with seed 104: (4, 4), logistic, 0.08, trial2
 
 
 
-
-# SINGLE HIDDEN LAYER
-# find optimum order of AR(p) model
+## SINGLE HIDDEN LAYER ##
 perform_neuralnet_ar_one <- function(seed, order, new_col_name) {
+  # write a function that does the same exact functionality as perform_neuralnet_ar_two()
+  # but with a single hidden layer
   scaled_exchange_data_shifted <- shift.column(scaled_exchange_data, columns = "rate", newNames = new_col_name, len = order, up = F)
   scaled_exchange_data_shifted <- scaled_exchange_data_shifted[c(-1)]
   train <- na.omit(scaled_exchange_data_shifted[0:400, ])
@@ -139,15 +136,16 @@ order_5_hidden_1 <- perform_neuralnet_ar_one(104, 5, "rate_5")
 # AR of order 1 gave best metrics
 
 
-
 # Optimal Input vector - AR(1)
 scaled_exchange_data_hidden_1_shifted <- shift.column(scaled_exchange_data, columns = "rate", newNames = "rate_1", len = 1, up = F)
 scaled_exchange_data_hidden_1_shifted <- scaled_exchange_data_hidden_1_shifted[c(-1)]
 train_hidden_1 <- na.omit(scaled_exchange_data_hidden_1_shifted[0:400, ])
 test_hidden_1 <- na.omit(scaled_exchange_data_hidden_1_shifted[401:500, ])
 
-# implement many neural networks with different hyper parameter values
+
 perfom_neuralnet_calculations_one <- function(seed, hidden, activation, learning) {
+  # write a function that does the same exact functionality as perform_neuralnet_calculations_two()
+  # but with a single hidden layer
   set.seed(seed) 
   nn_trial <- neuralnet(rate ~ rate_1, data = train_hidden_1, hidden = c(hidden), act.fct = activation, err.fct = "sse", lifesign = "full", learningrate = learning, rep = 10, linear.output = T)
   nn_trial.test_prediction <- predict(nn_trial, test_hidden_1)
@@ -174,12 +172,12 @@ trial18_metrics <- perfom_neuralnet_calculations_one(104, 4, "logistic", 0.1)
 
 # find optimal activation function for above best trial
 trial19_metrics <- perfom_neuralnet_calculations_one(104, 4, "tanh", 0.08)
-### Optimal values for 1 hidden layer network with seed 104: (4), logistic, 0.08, trial12
+# Optimal values for 1 hidden layer network with seed 104: (4), logistic, 0.08, trial12
 
 
 
 
-### BEST SINGLE HIDDEN LAYER NETWORK
+## BEST SINGLE HIDDEN LAYER NETWORK ##
 set.seed(104) 
 nn_best_single <- neuralnet(rate ~ rate_1, data = train_hidden_1, hidden = c(4), act.fct = "logistic", err.fct = "sse", lifesign = "full", learningrate = 0.08, rep = 10, linear.output = T)
 nn_best_single.test_prediction <- predict(nn_best_single, test_hidden_1)
@@ -188,7 +186,7 @@ plot(nn_best_single)
 
 
 
-### BEST TWO HIDDEN LAYER NETWORK
+## BEST TWO HIDDEN LAYER NETWORK ##
 set.seed(104)
 nn_best_double <- neuralnet(formula = rate ~ rate_1, data = train_hidden_2, hidden = c(4, 4), act.fct = "logistic", err.fct = "sse", lifesign = "full", learningrate = 0.08, rep = 10, linear.output = T)
 nn_best_double.test_prediction <- predict(nn_best_double, test_hidden_2)
@@ -197,7 +195,7 @@ plot(nn_best_double)
 
 
 
-# plot graph of best network
+## plot graph of best network ##
 plot(test_hidden_1[, 1], type = "l", col = "blue", lwd = 2, xlab = "index", ylab = "Rates")
 lines(nn_best_single.test_prediction, type = "l", col = "red", lwd = 2)
 title("AR(1) Time Series - Single Hidden Layer")
